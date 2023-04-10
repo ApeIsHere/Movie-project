665e7aac6d4104c3cd954584124f6c4e4b55c227
ApeIsHere <53047099+ApeIsHere@users.noreply.github.com>
2023-04-10 16:36:05 +0300

    MovieList Form is done

--- src/js/script.js
+++ src/js/script.js
@@ -14,55 +14,124 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Шар",
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+//Ждем загрузки структуры страницы 'DOMContentLoaded', чтобы не было ошибок
 
-//-------------------------------------------------------------------
-const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    //-------------------------------------------------------------------
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
 
 
-// 1) Удаляем рекламу на странице
-adv.forEach(item => {
-    item.remove();
+    // Реализуем работу формы по добавлению фильмов
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;           // .value полуаем значение введенного текста
+        const favorite = checkbox.checked;      // .checked получаем значение чекбокса
+
+        if (newFilm) {                          // проверяем на пустую строку условием
+
+            if(newFilm.length > 21) {           // обрезаем длинные названия фильмов
+                newFilm = `${newFilm.substring(0, 22)}...`
+            }
+
+            if (favorite) {                     // выводим сообщение favorite movie
+                console.log("You've added a favorite movie");
+            }
+
+            movieDB.movies.push(newFilm);       // добавляем новый фильм в датабазу
+            sortArray(movieDB.movies);          // сортируем по алфавиту
+
+            createMovieList(movieDB.movies, movieList); // перестраиваем список фильмов по новой
+        }
+
+        event.target.reset();                    // сбрасываем саму форму
+    })
+    
+    
+        // 1) Удаляем рекламу на странице
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        })
+    }  
+    
+   
+    const makeChanges = () => {
+        // 2) Меняем жанра фильма
+        genre.textContent = "драма";
+            
+        // 3) Меняем задний фон
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    }
+
+
+    const sortArray = (arr) => {
+        arr.sort(); // сортируем по алфавиту
+    }
+    
+    
+    // 4) Меняем фильмы на фильмы из базы данных
+    function createMovieList(films, parent) {
+        sortArray(films);           // вызываем сортировку прямо во время создания фильмов
+    
+        parent.innerHTML = "";  // сначала удалим статичную верстку, чтобы поставить туда свои
+    
+    // помещаем новые элементы в список фильмов
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i+1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });  
+        // += дополнительное присваивание 
+        // здесь использован чтобы ДОБАВИТЬ новый элемент к существующему, а не заменить его
+        // пример 
+        // a = a +1;
+        // a += 1;
+
+
+
+        // Удаляем фильмы по нажатию на корзинку
+
+        // После построения всего списка собираем все корзины и вешаем на них EventListener
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+           btn.addEventListener('click', () => {
+            btn.parentElement.remove();             // Удаляем родительский элемент со страницы
+            movieDB.movies.splice(i, 1);            //Splice (откуда начать удалять, сколько элементов удалить)
+            
+            
+            // Используем рекурсию (когда функция вызывает сама себя) и перестраиваем фильмы после удаления, 
+            // чтобы не нарушался порядок фильмов и цифра соответствовала
+            createMovieList(films, parent);
+           }); 
+        });
+    }
+
+    
+  
+    
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 })
 
-// 2) Меняем жанра фильма
-genre.textContent = "драма";
-
-// 3) Меняем задний фон
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-// 4) Меняем фильмы на фильмы из базы данных
-
-// сначала удалим статичную верстку, чтобы поставить туда свои
-movieList.innerHTML = "";
-
-// сортируем по алфавиту
-movieDB.movies.sort();
-
-// помещаем новые элементы в список фильмов
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i+1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;
-});
-
-// += дополнительное присваивание 
-// здесь использован чтобы ДОБАВИТЬ новый элемент к существующему, а не заменить его
-// пример 
-// a = a +1;
-// a += 1;
